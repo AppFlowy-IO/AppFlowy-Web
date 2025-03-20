0c65b7ab261f7e0fea7009353e7ce5ad457fac39
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-03-20 14:31:18 +0800

    chore: replace chevron arrows

--- src/components/quick-note/NoteHeader.tsx
+++ src/components/quick-note/NoteHeader.tsx
@@ -1,8 +1,7 @@
 import React, { useMemo } from 'react';
-import { ReactComponent as RightIcon } from '@/assets/arrow_right.svg';
+import { ReactComponent as LeftIcon } from '@/assets/icons/alt_arrow_left.svg';
 import { ReactComponent as OpenIcon } from '@/assets/full_view.svg';
 import { ReactComponent as CollapseIcon } from '@/assets/collapse_all_page.svg';
-
 import { ReactComponent as CloseIcon } from '@/assets/close.svg';
 
 import { IconButton } from '@mui/material';
@@ -10,7 +9,13 @@ import { useTranslation } from 'react-i18next';
 import { QuickNote } from '@/application/types';
 import { getTitle } from '@/components/quick-note/utils';
 
-function NoteHeader ({ note, onBack, onClose, expand, onToggleExpand }: {
+function NoteHeader({
+  note,
+  onBack,
+  onClose,
+  expand,
+  onToggleExpand,
+}: {
   onBack: () => void;
   onClose: () => void;
   expand?: boolean;
@@ -24,30 +29,22 @@ function NoteHeader ({ note, onBack, onClose, expand, onToggleExpand }: {
   }, [note, t]);
 
   return (
-    <div className={'flex items-center gap-4 w-full overflow-hidden'}>
-      <IconButton
-        onClick={onBack}
-        size={'small'}
-      >
-        <RightIcon className={'transform rotate-180'} />
+    <div className={'flex w-full items-center gap-4 overflow-hidden'}>
+      <IconButton onClick={onBack} size={'small'}>
+        <LeftIcon className={'h-5 w-5'} />
       </IconButton>
-      <div className={'pl-[24px] truncate text-center font-medium flex-1'}>
-        {title}
-      </div>
+      <div className={'flex-1 truncate pl-[24px] text-center font-medium'}>{title}</div>
       <IconButton
-        onMouseDown={e => e.preventDefault()}
-        onClick={e => {
+        onMouseDown={(e) => e.preventDefault()}
+        onClick={(e) => {
           e.currentTarget.blur();
           onToggleExpand?.();
         }}
         size={'small'}
       >
-        {expand ? <CollapseIcon className={'transform rotate-45'} /> : <OpenIcon />}
+        {expand ? <CollapseIcon className={'rotate-45 transform'} /> : <OpenIcon />}
       </IconButton>
-      <IconButton
-        onClick={onClose}
-        size={'small'}
-      >
+      <IconButton onClick={onClose} size={'small'}>
         <CloseIcon />
       </IconButton>
     </div>
