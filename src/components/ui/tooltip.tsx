a874791e17c4e0b01e09fd3a61749ddbfb5123f4
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-04-02 13:54:35 +0800

    chore: code cleanup and add flex col

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -31,7 +31,21 @@ function TooltipContent({
         data-slot='tooltip-content'
         sideOffset={sideOffset}
         className={cn(
-          'animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--radix-tooltip-content-transform-origin) text-balance shadow-tooltip z-50 w-fit rounded-400 bg-fill-primary px-3 py-2 text-sm text-text-quaternary',
+          // Animation behavior
+          'animate-in fade-in-0 zoom-in-95',
+          'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95',
+
+          // Slide-in effects based on tooltip position
+          'data-[side=bottom]:slide-in-from-top-2',
+          'data-[side=left]:slide-in-from-right-2',
+          'data-[side=right]:slide-in-from-left-2',
+          'data-[side=top]:slide-in-from-bottom-2',
+
+          // Styling and layout
+          'text-balance shadow-tooltip z-50 origin-[--radix-tooltip-content-transform-origin]',
+          'w-fit rounded-400 bg-fill-primary px-3 py-2 text-sm text-text-quaternary',
+          'flex flex-col',
+
           className
         )}
         {...props}
@@ -43,9 +57,7 @@ function TooltipContent({
 }
 
 function TooltipShortcut({ className, ...props }: React.ComponentProps<'span'>) {
-  return (
-    <span data-slot='tooltip-shortcut' className={cn('ml-auto text-sm text-text-secondary', className)} {...props} />
-  );
+  return <span data-slot='tooltip-shortcut' className={cn('text-text-secondary', className)} {...props} />;
 }
 
 export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider, TooltipShortcut };
