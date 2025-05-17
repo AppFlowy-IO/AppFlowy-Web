dd8529b8b85655e10766c76742f49bc7dcedd4a7
lumixraku <lumixraku@gmail.com>
2025-05-17 17:12:06 +0800

    feat: autofix unused import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,6 @@ module.exports = {
     tsconfigRootDir: __dirname,
     extraFileExtensions: ['.json'],
   },
-  // Make sure to include the unused-imports plugin here
   plugins: ['@typescript-eslint', 'react-hooks', 'import', 'unused-imports'],
   settings: {
     'import/resolver': {
@@ -67,12 +66,7 @@ module.exports = {
     'no-void': 'off',
     'prefer-const': 'error',
     'prefer-spread': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-      },
-    ],
+
     'padding-line-between-statements': [
       'error',
       { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
@@ -86,7 +80,6 @@ module.exports = {
     'import/no-named-as-default': 'off',
     'import/no-named-as-default-member': 'off',
 
-    // 添加 import 规则（设置为 warn 而不是 error）
     'import/no-unresolved': 'warn',
     'import/named': 'warn',
     'import/namespace': 'warn',
@@ -95,13 +88,10 @@ module.exports = {
     'import/no-unused-modules': 'warn',
     'import/no-duplicates': 'warn',
 
-    // Remove this line
-    // 'import/no-unused-imports': 'warn',
-
-    // Add these rules for the unused-imports plugin
-    'unused-imports/no-unused-imports': 'warn',
+    // unused-imports should be error level so eslint can auto fix it
+    'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
-      'warn',
+      'error',
       {
         vars: 'all',
         varsIgnorePattern: '^_',
@@ -109,8 +99,13 @@ module.exports = {
         argsIgnorePattern: '^_'
       }
     ],
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      vars: 'all',
+      varsIgnorePattern: '^_',
+      args: 'after-used',
+      argsIgnorePattern: '^_'
+    }],
 
-    // import 顺序规则
     'import/order': ['warn', {
       'groups': [
         'builtin',
